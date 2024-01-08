463378596320e28db9df7bad2c905009b47e58fa
Shardendu Jha <appujha0@gmail.com>
2024-01-08 07:13:37 +0000

    device status style create

--- seperate.py
+++ seperate.py
@@ -25,19 +25,19 @@ def process_device_info():
     if response.status_code == 200:
         devices = response.json().get('items', [])
         device_info.extend(devices)
-        for item in devices:
-            print(f"Label: {item.get('label')}")
-            print(f"Manufacturer Name: {item.get('manufacturerName')}")
-            print(f"Type: {item.get('type')}")
-            print(f"Create Time: {item.get('createTime')}")
-            print("\n")
+        # for item in devices:
+            # print(f"Label: {item.get('label')}")
+            # print(f"Manufacturer Name: {item.get('manufacturerName')}")
+            # print(f"Type: {item.get('type')}")
+            # print(f"Create Time: {item.get('createTime')}")
+            # print("\n")
 
-            viper_info = item.get('viper', {})
-            print(f"Viper Manufacturer: {viper_info.get('manufacturerName')}")
-            print(f"Model Name: {viper_info.get('modelName')}")
-            print(f"Software Version: {viper_info.get('swVersion')}")
-            print(f"Hardware Version: {viper_info.get('hwVersion')}")
-            print("\n")
+            # viper_info = item.get('viper', {})
+            # print(f"Viper Manufacturer: {viper_info.get('manufacturerName')}")
+            # print(f"Model Name: {viper_info.get('modelName')}")
+            # print(f"Software Version: {viper_info.get('swVersion')}")
+            # print(f"Hardware Version: {viper_info.get('hwVersion')}")
+            # print("\n")
 
     data_dict = tapo_socket.check_device_status(accesstoken_smartthings, deviceid)
     switch_status, switch_timestamp = extract_switch_status_and_timestamp(data_dict)
@@ -50,7 +50,7 @@ def extract_switch_status_and_timestamp(d):
     return value, timestamp
 
 # Usage example
-device_info, switch_status, switch_timestamp = process_device_info()
-print("Device Information:", device_info)
-print("Switch Status:", switch_status)
-print("Switch Timestamp:", switch_timestamp)
+# device_info, switch_status, switch_timestamp = process_device_info()
+# print("Device Information:", device_info)
+# print("Switch Status:", switch_status)
+# print("Switch Timestamp:", switch_timestamp)
